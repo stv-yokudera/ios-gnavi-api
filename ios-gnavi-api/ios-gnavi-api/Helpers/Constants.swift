0c8e67c7c608fc5f3b60ca97efc64fcdf66ab358
OkuderaYuki <>
2018-09-22 18:41:40 +0900

    Implemented json file reader.

---
+++ ios-gnavi-api/ios-gnavi-api/Helpers/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  ios-gnavi-api
+//
+//  Created by YukiOkudera on 2018/09/22.
+//  Copyright © 2018 YukiOkudera. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+
+    private init() {}
+    static let areaJsonFileName = "area.json"
+}
